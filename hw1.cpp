1aa58b70fd14aafc62c0e796884a52abaa0b1261
ZacharyMorganUark <145289212+ZacharyMorganUark@users.noreply.github.com>
2024-02-12 17:37:45 -0600

    Create hw1.cpp

---
+++ hw1.cpp
@@ -0,0 +1,95 @@
+//---------------------------------------
+//hw1.cpp
+//Zachary Morgan
+//---------------------------------------
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef MAC
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+// Maximum number of fireworks
+const int MIN_FIREWORKS = 5;
+const int MAX_FIREWORKS = 10;
+
+//---------------------------------------
+// Function to generate a random color
+//---------------------------------------
+void set_random_color()
+{
+    glColor3f((rand() % 100) / 100.0, (rand() % 100) / 100.0, (rand() % 100) / 100.0);
+}
+
+//---------------------------------------
+// Function to draw random fireworks
+//---------------------------------------
+void draw_fireworks()
+{
+    int numFireworks = rand() % (MAX_FIREWORKS - 4) + MIN_FIREWORKS; // Random number of fireworks [5..10]
+
+    for (int i = 0; i < numFireworks; i++)
+    {
+        float size = (rand() % 50) / 100.0 + 0.5; // Random size between 0.5 and 1.0
+        float x = (rand() % 200) / 100.0 - 1.0;    // Random x location between -1.0 and 1.0
+        float y = (rand() % 200) / 100.0 - 1.0;    // Random y location between -1.0 and 1.0
+
+        set_random_color(); // Set a random color for the firework
+        glBegin(GL_POINTS);
+        glVertex2f(x, y);
+        glEnd();
+
+        glBegin(GL_LINES);
+        for (int j = 0; j < 36; j++)
+        {
+            float angle = j * 10.0 * (M_PI / 180.0); // Convert degrees to radians
+            float dx = size * cos(angle);
+            float dy = size * sin(angle);
+            glVertex2f(x, y);
+            glVertex2f(x + dx, y + dy);
+        }
+        glEnd();
+    }
+}
+
+//---------------------------------------
+// Display callback for OpenGL
+//---------------------------------------
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    draw_fireworks();
+    glFlush();
+}
+
+//---------------------------------------
+// Init function for OpenGL
+//---------------------------------------
+void init()
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+//---------------------------------------
+// Main program
+//---------------------------------------
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(250, 250);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutCreateWindow("Random Fireworks");
+    glutDisplayFunc(display);
+    init();
+    glutMainLoop();
+    return 0;
+}
