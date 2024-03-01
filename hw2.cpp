55865731f614270d5393d0b03a908aac964b474d
ZacharyMorganUark <145289212+ZacharyMorganUark@users.noreply.github.com>
2024-03-01 02:25:16 -0600

    Update hw2.cpp

--- hw2.cpp
+++ hw2.cpp
@@ -1,124 +1,88 @@
-#include <cmath>
-#include <ctime>
-#include <cstdio>
-#include <cstdlib>
-
-#ifdef MAC
-#include <GLUT/glut.h>
-#else
 #include <GL/glut.h>
-#endif
+#include <cmath>
 
-//4500
-
-const int MAX_VERTICES = 4;
-const int LINE_COUNT = 100;
-const int MOVEMENT_DURATION = 5000;
-
-float squareVertices[MAX_VERTICES][2] = {
-    {-0.1, -0.1},
-    {0.1, -0.1},
-    {0.1, 0.1},
-    {-0.1, 0.1}
-};
-
-float pathVertices[MAX_VERTICES * LINE_COUNT][2];
-int vertexCount = 0;
-bool drawing = false;
-std::clock_t moveStartTime;
+GLfloat squareSize = 50.0f;
+GLfloat squarePositionX = 0.0f;
+GLfloat squarePositionY = 0.0f;
+bool drawLine = false;
+GLfloat lineStartX, lineStartY, lineEndX, lineEndY;
 
 void drawSquare() {
-    glBegin(GL_POLYGON);
-    for (int i = 0; i < MAX_VERTICES; ++i) {
-        glVertex2f(squareVertices[i][0], squareVertices[i][1]);
-    }
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue color
+    glBegin(GL_QUADS);
+    glVertex2f(squarePositionX, squarePositionY);
+    glVertex2f(squarePositionX + squareSize, squarePositionY);
+    glVertex2f(squarePositionX + squareSize, squarePositionY + squareSize);
+    glVertex2f(squarePositionX, squarePositionY + squareSize);
     glEnd();
 }
 
-void drawPath() {
-    glBegin(GL_LINE_STRIP);
-    for (int i = 0; i < vertexCount; ++i) {
-        glVertex2f(pathVertices[i][0], pathVertices[i][1]);
-    }
+void drawLineSegment(GLfloat startX, GLfloat startY, GLfloat endX, GLfloat endY) {
+    glColor3f(1.0f, 0.0f, 0.0f); // Red color
+    glBegin(GL_LINES);
+    glVertex2f(startX, startY);
+    glVertex2f(endX, endY);
     glEnd();
-}
-
-void mouseClick(int button, int state, int x, int y) {
-    if (button == GLUT_LEFT_BUTTON) {
-        if (state == GLUT_DOWN) {
-            drawing = true;
-            vertexCount = 0;
-        } else if (state == GLUT_UP) {
-            drawing = false;
-            moveStartTime = std::clock();  // Start animation
-        }
-    }
-}
-
-void mouseMotion(int x, int y) {
-    if (drawing && vertexCount < MAX_VERTICES * LINE_COUNT) {
-        float xCoord = (float)x / glutGet(GLUT_WINDOW_WIDTH) * 2 - 1;
-        float yCoord = 1 - (float)y / glutGet(GLUT_WINDOW_HEIGHT) * 2;
-        pathVertices[vertexCount][0] = xCoord;
-        pathVertices[vertexCount][1] = yCoord;
-        ++vertexCount;
-        glutPostRedisplay();
-    }
-}
-
-void moveSquare() {
-    glPushMatrix();
-
-    if (vertexCount > 1) {
-        std::clock_t currentTime = std::clock();
-        double elapsedTime = (double)(currentTime - moveStartTime) / CLOCKS_PER_SEC;
-
-        float t = fmin(1.0, elapsedTime / (MOVEMENT_DURATION / 1000.0));
-        float currentX = (1 - t) * pathVertices[0][0] + t * pathVertices[vertexCount - 1][0];
-        float currentY = (1 - t) * pathVertices[0][1] + t * pathVertices[vertexCount - 1][1];
-
-        glTranslatef(currentX, currentY, 0.0);
-    }
-
-    drawSquare();
-
-    glPopMatrix();
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
 
-    if (drawing) {
-        drawPath();
-    } else {
-        drawSquare();
-        moveSquare();
+    drawSquare();
+
+    if (drawLine) {
+        drawLineSegment(lineStartX, lineStartY, lineEndX, lineEndY);
     }
 
     glutSwapBuffers();
 }
 
-void init() {
-    glClearColor(0.0, 0.0, 0.0, 1.0); // Set clear color to black
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+void keyboard(unsigned char key, int x, int y) {
+    if (key == 'M' || key == 'm') {
+        // Move the square along the line
+        squarePositionX = lineEndX;
+        squarePositionY = lineEndY;
+    }
 }
 
-int main(int argc, char *argv[]) {
+void mouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        if (!drawLine) {
+            // Record the starting point of the line
+            lineStartX = x;
+            lineStartY = glutGet(GLUT_WINDOW_HEIGHT) - y;
+            drawLine = true;
+        } else {
+            // Record the ending point of the line
+            lineEndX = x;
+            lineEndY = glutGet(GLUT_WINDOW_HEIGHT) - y;
+            drawLine = false;
+        }
+    }
+    glutPostRedisplay();
+}
+
+void reshape(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, width, 0, height);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitWindowSize(500, 500);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutCreateWindow("Drawing and Moving Square");
+    glutCreateWindow("OpenGL Square and Line");
+    glutReshapeWindow(800, 600);
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
     glutDisplayFunc(display);
-    glutMouseFunc(mouseClick);
-    glutMotionFunc(mouseMotion);
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
+    glutReshapeFunc(reshape);
 
-    init();
     glutMainLoop();
-
     return 0;
 }
