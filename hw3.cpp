9039b003783259645da95405f06ff1a91c8a1ec0
ZacharyMorganUark <145289212+ZacharyMorganUark@users.noreply.github.com>
2024-03-24 23:28:34 -0500

    Update hw3.cpp

--- hw3.cpp
+++ hw3.cpp
@@ -5,11 +5,17 @@ const int WIDTH = 500;
 const int HEIGHT = 500;
 float depthValues[WIDTH][HEIGHT];
 
+float x_angle = 0.0f;
+float y_angle = 0.0f;
+float z_angle = 0.0f;
+
 void init() {
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0, WIDTH, 0, HEIGHT, -1, 1);
+    gluPerspective(45.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 void readDepthData() {
@@ -28,8 +34,13 @@ void readDepthData() {
 }
 
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
+
+    glTranslatef(0.0f, 0.0f, -5.0f);
+    glRotatef(x_angle, 1.0f, 0.0f, 0.0f);
+    glRotatef(y_angle, 0.0f, 1.0f, 0.0f);
+    glRotatef(z_angle, 0.0f, 0.0f, 1.0f);
 
     glBegin(GL_QUADS);
     for (int i = 0; i < WIDTH - 1; ++i) {
@@ -45,9 +56,35 @@ void display() {
     glutSwapBuffers();
 }
 
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'x':
+            x_angle += 5.0f;
+            break;
+        case 'X':
+            x_angle -= 5.0f;
+            break;
+        case 'y':
+            y_angle += 5.0f;
+            break;
+        case 'Y':
+            y_angle -= 5.0f;
+            break;
+        case 'z':
+            z_angle += 5.0f;
+            break;
+        case 'Z':
+            z_angle -= 5.0f;
+            break;
+        default:
+            break;
+    }
+    glutPostRedisplay();
+}
+
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(WIDTH, HEIGHT);
     glutCreateWindow("Penny Model");
 
@@ -55,6 +92,10 @@ int main(int argc, char** argv) {
     readDepthData();
 
     glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard); // Register keyboard callback
+
+    glEnable(GL_DEPTH_TEST);
+
     glutMainLoop();
     return 0;
 }
