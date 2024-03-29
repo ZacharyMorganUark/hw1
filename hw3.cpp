7792f740c2cda8ed347ea06bd6759ce25db919f3
ZacharyMorganUark <145289212+ZacharyMorganUark@users.noreply.github.com>
2024-03-29 16:40:47 -0500

    Update hw3.cpp

--- hw3.cpp
+++ hw3.cpp
@@ -1,3 +1,9 @@
+//---------------------------------------
+// Program: hw3.cpp using balls.cpp as skeleton
+// Purpose: Penny model with Phong shading
+// Author:  Zachary Morgan
+// Date:    29 March 2024
+//---------------------------------------
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +13,7 @@
 
 #define ROWS 500
 #define COLS 500
-#define SCALE_FACTOR 0.0002
+#define SCALE_FACTOR 0.0002 //penny depth size
 
 float Depth[ROWS][COLS];
 float R[ROWS][COLS], G[ROWS][COLS], B[ROWS][COLS];
@@ -22,7 +28,7 @@ void read_depth_data(const char *filename) {
         exit(1);
     }
 
-    for (int i = ROWS - 1; i >= 0; i--) {  // Read rows in reverse order
+    for (int i = ROWS - 1; i >= 0; i--) { //reverse order ?
         for (int j = 0; j < COLS; j++) {
             if (fscanf(file, "%f", &Depth[i][j]) != 1) {
                 fprintf(stderr, "Error reading depth data from file\n");
@@ -54,21 +60,21 @@ void read_color_data(const char *filename) {
 }
 
 void get_surface_normal(int i, int j, float& normal_x, float& normal_y, float& normal_z) {
-    // Calculate the surface normal using cross product of vectors formed by adjacent vertices
+    // calculate with cross product
     float vertex1[3] = {(float)j / COLS - 0.5, (float)i / ROWS - 0.5, Depth[i][j] * SCALE_FACTOR};
     float vertex2[3] = {(float)(j + 1) / COLS - 0.5, (float)i / ROWS - 0.5, Depth[i][j + 1] * SCALE_FACTOR};
     float vertex3[3] = {(float)(j + 1) / COLS - 0.5, (float)(i + 1) / ROWS - 0.5, Depth[i + 1][j + 1] * SCALE_FACTOR};
 
-    // Calculate vectors from vertex1 to vertex2 and vertex3
+    // get vectors
     float vector1[3] = {vertex2[0] - vertex1[0], vertex2[1] - vertex1[1], vertex2[2] - vertex1[2]};
     float vector2[3] = {vertex3[0] - vertex1[0], vertex3[1] - vertex1[1], vertex3[2] - vertex1[2]};
 
-    // Calculate the cross product of the two vectors to get the surface normal
+    // calculate vector cross product
     normal_x = vector1[1] * vector2[2] - vector1[2] * vector2[1];
     normal_y = vector1[2] * vector2[0] - vector1[0] * vector2[2];
     normal_z = vector1[0] * vector2[1] - vector1[1] * vector2[0];
 
-    // Normalize the surface normal
+    // convert normals
     float length = sqrt(normal_x * normal_x + normal_y * normal_y + normal_z * normal_z);
     normal_x /= length;
     normal_y /= length;
@@ -222,6 +228,14 @@ int main(int argc, char *argv[]) {
     glutCreateWindow("Penny Model");
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
+    printf("  'x' = decrease X translation/rotation\n");
+    printf("  'X' = increase X translation/rotation\n");
+    printf("  'y' = decrease Y translation/rotation\n");
+    printf("  'Y' = increase Y translation/rotation\n");
+    printf("  'z' = decrease Z translation/rotation\n");
+    printf("  'Z' = increase Z translation/rotation\n");
+    printf("  '2' = change to color display mode\n");
+    printf("  '3' = change to phong shading mode\n");
     init();
     glutMainLoop();
     return 0;
