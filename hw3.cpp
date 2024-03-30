f1ba54161ce15d130565a750d62cf5d260660e31
ZacharyMorganUark <145289212+ZacharyMorganUark@users.noreply.github.com>
2024-03-29 19:13:41 -0500

    Update hw3.cpp

--- hw3.cpp
+++ hw3.cpp
@@ -109,10 +109,10 @@ void display() {
     glRotatef(z_angle, 0, 0, 1);
 
 
-    glColor3f(0.0, 0.0, 0.0); // Outline color (black)
-    glLineWidth(2.0); // Set the line width for the outline
+    //glColor3f(0.0, 0.0, 0.0); // Outline color (black)
+    //glLineWidth(2.0); // Set the line width for the outline
 
-    glColor3f(1.0, 1.0, 1.0);
+    glColor3f(0.0, 0.0, 0.0);
     for (int i = 0; i < ROWS - 1; i++) {
         glBegin(GL_LINE_LOOP);
         for (int j = 0; j < COLS - 1; j++) {
